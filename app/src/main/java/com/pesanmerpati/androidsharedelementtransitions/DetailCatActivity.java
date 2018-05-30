c0260bc6372c3cfa6d982cc34cfa3dce4689340c
rohmts <rohmatsuseno@gmail.com>
2018-05-30 23:56:58 +0700

    Add shared element activity to activity

---
+++ app/src/main/java/com/pesanmerpati/androidsharedelementtransitions/DetailCatActivity.java
@@ -0,0 +1,13 @@
+package com.pesanmerpati.androidsharedelementtransitions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailCatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_cat);
+    }
+}
--- app/src/main/java/com/pesanmerpati/androidsharedelementtransitions/MainActivity.java
+++ app/src/main/java/com/pesanmerpati/androidsharedelementtransitions/MainActivity.java
@@ -1,13 +1,35 @@
 package com.pesanmerpati.androidsharedelementtransitions;
 
+import android.content.Intent;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImageViewCat;
+    private TextView mTextViewDesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageViewCat = findViewById(R.id.imageViewCat);
+        mTextViewDesc = findViewById(R.id.textViewDesc);
+
+        mTextViewDesc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentDetail = new Intent(MainActivity.this, DetailCatActivity.class);
+                ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        MainActivity.this, mImageViewCat, ViewCompat.getTransitionName(mImageViewCat));
+                startActivity(intentDetail, optionsCompat.toBundle());
+            }
+        });
     }
 }
